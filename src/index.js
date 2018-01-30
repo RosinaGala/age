cc68789c66367b4e7233e6c2425c65d243815b8e
Sina Burow <sina.burow@sinnerschrader.com>
2018-01-30 17:27:44 +0100

    added index.js

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const { DateTime, Interval } = require('luxon')
+
+const container = document.getElementById('age');
+
+calculateAge = () => {
+    const durationOfLife = calculateDurationOfLife();
+    const result = formatDurationWithSup(durationOfLife);
+    
+    container.innerHTML = result;
+};
+
+calculateDurationOfLife = () => {
+    const now =  DateTime.local();
+    const born = DateTime.local(1989, 10, 26);
+    return Interval.fromDateTimes(born, now)
+};
+
+formatDurationWithSup = (dol) => {
+    const fullAgeAsString = dol.length('years', true).toString();
+    const years = fullAgeAsString.substring(0, 2);
+    const theRest = fullAgeAsString.substring(2, 11);
+
+    return years + theRest.sup()
+};
+
+window.setInterval(calculateAge, 100);
